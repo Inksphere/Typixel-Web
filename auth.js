533842f2f9401e6d7a812a0bdea8994cb593b054
TyPixel.Co <hashimmhdpang@gmail.com>
2025-12-18 10:05:46 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function(){
+  signInWithEmailAndPassword(auth, email.value, password.value)
+  .then(() => {
+    window.location.href = "dashboard.html";
+  })
+  .catch(err => alert(err.message));
+};
